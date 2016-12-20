39a20a101075322b917adda3c0fd0c9322b531e0
JanGalante <ave_caesar@hotmail.com>
2016-12-20 20:30:35 +0100

    Adding Open npm package to open url in application

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,21 +1,26 @@
-// var path = require("path");
-// var express = require('express')
-
-
 import express from 'express';
-import path from 'path';
-// import open from 'open';
+import path from 'path'; //global access to path set in Node
+import open from 'open';
 // import webpack from 'webpack';
 // import config from '../webpack.config.dev';
 
-
+const port = 3000;
 const app = express()
+
 
 app.get('/', function (req, res) {
     //res.send('Hello World!')
     res.sendFile(path.join(__dirname, '../src/index.html'));
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+// app.listen(3000, function () {
+//   console.log('Example app listening on port 3000!')
+// })
+
+app.listen(port, function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    open('http://localhost:' + port);
+  }
+});
