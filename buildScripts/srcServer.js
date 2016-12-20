d347c770322108717ce777fb050ff4d23e8bd930
JanGalante <ave_caesar@hotmail.com>
2016-12-20 20:24:35 +0100

    Making express server work

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,47 +1,21 @@
+// var path = require("path");
+// var express = require('express')
 
-// import express from 'express';
-// import path from 'path';
+
+import express from 'express';
+import path from 'path';
 // import open from 'open';
-
-// var port = 3000;
-// var app = express(); //create an instance of express
-
-// //define route
-// app.get('/', function(req, resp){
-//     res.sendFile(path.join(__dirname, '../src/index.html'))
-// });
-
-// //tell express to listen to the port
-// app.listen(port, function(err){
-//     if(err){
-//         console.log(err);
-//     }
-//     else{
-//         open('http://localhost:' + port)
-//     }
-// });
+// import webpack from 'webpack';
+// import config from '../webpack.config.dev';
 
 
+const app = express()
 
-// import express from 'express';
-// import https from 'https';
-// import http from 'http';
+app.get('/', function (req, res) {
+    //res.send('Hello World!')
+    res.sendFile(path.join(__dirname, '../src/index.html'));
+})
 
-// const port = 3000;
-// const app = express();
-
-// //GET method route
-// app.get('/', function (req, res) {
-//   res.send('GET request to the homepage')
-// })
-
-// //define route
-// // app.get('/', function(req, resp){
-// //     res.sendFile(path.join(__dirname, '../src/index.html'))
-// // });
-
-// http.createServer(app).listen(80, function(err){
-//     if(err){
-//         console.log(err);
-// }});
-// // https.createServer(options, app).listen(443);
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
