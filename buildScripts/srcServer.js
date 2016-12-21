6d14d2984348afe6dffeeb61ad52399be3eecb97
JanGalante <ave_caesar@hotmail.com>
2016-12-21 15:07:04 +0100

    Configure webpack with express

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -4,18 +4,22 @@ import open from 'open';
 import webpack from 'webpack';
 import config from '../webpack.config.dev';
 
+
+
 const port = 3000;
 const app = express()
+const compiler = webpack(config); //new webpack compiler
 
+//Tell express to use the webpack middleware
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath //use the publicPath in our config
+}));
 
 app.get('/', function (req, res) {
     //res.send('Hello World!')
     res.sendFile(path.join(__dirname, '../src/index.html'));
 })
-
-// app.listen(3000, function () {
-//   console.log('Example app listening on port 3000!')
-// })
 
 app.listen(port, function(err) {
   if (err) {
