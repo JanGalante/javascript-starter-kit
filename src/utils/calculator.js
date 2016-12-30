111ef370d0f0d3fd160aa1d9a0cdb545dfb3916b
JanGalante <ave_caesar@hotmail.com>
2016-12-30 20:51:28 +0100

    Extract logic to POJOs

---
+++ src/utils/calculator.js
@@ -0,0 +1,4 @@
+//Extract logic to POJO:s (Plain old JavaScript objects).
+
+//POJO:s contains plain logic not tied to any framework. Strive to place as much logic as possiblen in plain JavaScript. Makes it it esier to test and reuse.
+//Minimize the effort to change to another framework. Not unrealistic that it might happen in the fast changing JavaScript world.
