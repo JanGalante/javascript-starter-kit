9a90f4e349ac3a1bd6eca79abc1adb1509ca00b8
JanGalante <ave_caesar@hotmail.com>
2017-01-02 20:18:34 +0100

    Production webpack

---
+++ webpack.config.prod.js
@@ -0,0 +1,23 @@
+import path from 'path';
+
+export default {
+  debug: true,
+  devtool: 'source-map', //highest quality sourcemaps
+  noInfo: false,
+  entry: [
+    path.resolve(__dirname, 'src/index')
+  ],
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'dist'), //write to distribution folder
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [],
+  module: {
+    loaders: [
+      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
+      {test: /\.css$/, loaders: ['style','css']}
+    ]
+  }
+}
